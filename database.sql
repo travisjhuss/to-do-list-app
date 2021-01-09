2348e4788f9193cc41880cf9069f7f6424a668bb
Travis Huss <travisjhuss@gmail.com>
2021-01-09 09:42:59 -0600

    database.sql updated with table info and dummy data

--- database.sql
+++ database.sql
@@ -11,14 +11,22 @@
     -- time = varchar(10) default null
     -- completed = boolean default false
 
+-- to create table
 CREATE TABLE "tasks" (
 
     "id" SERIAL PRIMARY key,
     "task" VARCHAR(200) NOT NULL,
-    "priority" VARCHAR(10) DEFAULT "low",
+    "priority" VARCHAR(10) DEFAULT 'low',
     "label" VARCHAR(20) DEFAULT NULL,
     "date" VARCHAR(12) DEFAULT NULL,
     "time" VARCHAR(10) DEFAULT NULL,
     "completed" BOOLEAN DEFAULT false
 
-)
+);
+
+-- dummy data
+INSERT INTO "tasks" ("task", "priority", "label", "date", "time")
+VALUES ('Take out the trash', 'low', 'home', default, default), 
+('Prepare presentation', 'high', 'work', '2021-01-16', '10:30'),
+('Buy cake for Katelyn''s birthday', 'medium', 'home', '2021-01-12', '15:00'),
+('Complete weekend assignment', 'high', 'school', '2021-01-10', '17:00');
